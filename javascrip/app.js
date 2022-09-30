55b38d38305d4273afaf8a962a097378d4e49c21
YuvCarl <carldrake969@gmail.com>
2022-09-30 10:40:32 +0100

    commited

---
+++ javascrip/app.js
@@ -0,0 +1,15 @@
+let Name;
+Name = "hello world"
+console.log(Name)
+alert(Name)
+document.write(Name)
+
+let perosn = {
+    name: "firstname",
+    class: "class one",
+    autho: "the writer"
+}
+console.table(perosn)
+function Change(){
+    document.getElementById("script").innerHTML = ("the test has been written ussing js")
+}
